7a099e852727619f1ee2e8bdf06b953ba5a4b6ba
Ehiber <ehiber5@hotmail.com>
2019-12-18 17:29:47 -0400

    subiendo home

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,13 +3,43 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-expand-md navbar-dark bg-dark">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">LOGO</span>
 			</Link>
+
+			<button
+				className="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarNavAltMarkup"
+				aria-controls="navbarNavAltMarkup"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon" />
+			</button>
+
+			<div className="collapse navbar-collapse" id="navbarNav">
+				<div className="navbar-nav list-group-horizontal">
+					<Link className="nav-item nav-link mx-3 active" to="/">
+						Juegos
+					</Link>
+					<Link className="nav-item nav-link mx-3 active" to="/">
+						Torneos
+					</Link>
+					<Link className="nav-item nav-link mx-3 active" to="/">
+						Otra Vista
+					</Link>
+				</div>
+			</div>
+
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-success">Registrate</button>
+				</Link>
+
+				<Link to="/demo">
+					<button className="btn btn-primary">Ingresa</button>
 				</Link>
 			</div>
 		</nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,34 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import "../../styles/home.scss";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+	<Fragment>
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col welcome-block">
+					<h1>LOGO</h1>
+					<p> Little Description KLJDFLKJFASKLDFJLÑKADSF </p>
+					<Link to="/demo">
+						<button className="btn btn-primary">UNETE YA</button>
+					</Link>
+				</div>
+			</div>
+		</div>
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col second-block">SELECCIONA TU JUEGO</div>
+			</div>
+		</div>
+		<div className="container-fluid">
+			<div className="row">
+				<div className="third-block">TORNEOS COMO LOS QUIERAS</div>
+			</div>
+		</div>
+		<div className="container-fluid">
+			<div className="row">
+				<div className="fourth-block">DINERO REAL</div>
+			</div>
+		</div>
+	</Fragment>
 );
